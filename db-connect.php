23735861c91eebc8da4667ab95cfb2effe0998d6
David Chau <davidc2509@gmail.com>
2023-01-09 09:56:29 +0100

    🔧Added db config file

---
+++ db-connect.php
@@ -0,0 +1,14 @@
+<?php
+    $host="localhost";
+    $username="root";
+    $password="toto";
+    $dbname="gc-project";
+    try{
+        $db =new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
+        //echo "Connexion ok";
+    }
+    catch(ErrorException $e){
+        echo $e;
+    }
+
+?>
