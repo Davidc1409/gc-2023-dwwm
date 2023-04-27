60654ff390f74e6aef43a433fdb3e373f42318c8
Davidc1409 <davidc2509@gmail.com>
2023-04-27 14:52:33 +0200

    :sparkles: Added updated mailer module file

---
+++ php/mailer.php
@@ -0,0 +1,30 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require("vendor/autoload.php");
+
+function mailer($to,$subject,$body){
+    $mail = new PHPMailer();
+    try{
+    $mail->IsSMTP();
+    $mail->SMTPDebug = 0;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = "tls";
+    $mail->Host = "smtp-mail.outlook.com";
+    $mail->Port = 587;
+    $mail->Username = "david.chau@imie-paris.fr";
+    $mail->Password = "Dqvid8725";
+    $mail->SetFrom("david.chau@imie-paris.fr","David Chau");
+    $mail->Subject = $subject;
+    $mail->Body = $body;
+    $mail->AddAddress($to);
+
+    $mail->send();
+
+    } catch (Exception $e){
+        echo "error : $e";
+
+    }
+}
+
+?>
