882e7e29a8268b08de22a367211cb0ec30b89073
Davidc1409 <davidc2509@gmail.com>
2023-04-27 14:40:42 +0200

    :sparkles: Added updated Authentification js file

---
+++ utils/auth.js
@@ -0,0 +1,34 @@
+let user = JSON.parse(localStorage.getItem("user"));
+
+if (!user) {
+    window.location.replace("../login/login.html");
+}
+
+if (user){
+    $("#btn-connect").addClass("d-none");
+    $.ajax({
+        url:"/gc-2023-DWWM/php/user.php",
+        type: "GET",
+        dataType: "json",
+        data:{
+            choice: "select_id_users",
+            id: user.id
+        },
+        success:(res) => {
+            if(res.success){
+                $("#profil-avatar").attr("src","/gc-2023-DWWM"+res.user.avatar);
+            }
+        }
+    })
+    if(user.user_rights==1){
+        const li=$("<li></li>");
+        const link=$("<a></a>").text("Admin");
+        link.attr("href", "../admin/articles/articles_dashboard.html");
+        link.addClass("dropdown-item");
+        li.append(link);
+        $("#dropdown-profil-update").after(li);
+
+    }
+}
+
+
